7068097e294b2a6f608b97359ea9d5eb4a4b1c0a
Charin Tantrakul <charin.ta@ku.th>
2017-02-05 22:11:52 +0700

    ArrayIterator

---
+++ README.md
@@ -0,0 +1,5 @@
+##ArrayIterator
+###by Charin Tantrakul
+
+Many Collections and data structures provide an Iterator but not in array
+.So in ArrayIterator you can iterate all the elements in array.
---
+++ src/ku/util/ArrayIterator.java
@@ -0,0 +1,43 @@
+package ku.util;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+public class ArrayIterator<T> implements Iterator<T>  {
+	/** attribute for the array we want to iterate over */
+	private T[ ] array;
+	/*
+	 * Set cursor as array index
+	 */
+	private int cursor;
+	
+	/**
+	 * Initialize a new array iterator with the array to process.
+	 * @param array is the array to iterate over
+	 */
+	public ArrayIterator(T[] array) {
+		this.array = array;
+		this.cursor = -1 ;        
+	}
+
+	/**
+	 * Return the next non-null element from array, if any.
+	 * @return the next non-null element in the array.
+	 * @throws NoSuchElementException if there are no more elements
+	 *        to return.
+	 */
+	public T next( ) {
+		if(hasNext()==true){
+			cursor++;
+			if(array[cursor]!=null){
+				return array[cursor];
+			}
+		}
+		throw new NoSuchElementException();
+	}
+
+	/**
+	 * Check next element from array that is non-null element or not.
+	 */
+	public boolean hasNext(){
+		return cursor != array.length-1;
+	}
+}
